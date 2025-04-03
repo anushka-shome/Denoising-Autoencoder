51f14625adf57c24e90558e2580281aff7daf4f6
Michael Gribskov <mgribsko@gmail.com>
2025-04-03 11:10:12 -0400

    kmeans.py added 2D plot and legend

--- kmeans.py
+++ kmeans.py
@@ -7,69 +7,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-
-class Group:
-    """=============================================================================================
-
-
-    ============================================================================================="""
-    group_n = 0
-
-    def __init__(self, id=0):
-        """-----------------------------------------------------------------------------------------
-
-        -----------------------------------------------------------------------------------------"""
-        self.id = id if id else Group.group_n
-        Group.group_n += 1
-        self.values = []
-        self.centroid = []
-
-    def get_centroid(self):
-        """-----------------------------------------------------------------------------------------
-        calculate centroid position from values
-
-        :return:
-        -----------------------------------------------------------------------------------------"""
-        n = len(self.values)
-        if n == 0:
-            self.centroid = [0.0 for _ in self.values[0]['value']]
-            return self.centroid
-
-        sum = [0.0 for _ in self.values[0]['value']]
-        for v in self.values:
-            for i in range(len(v['value'])):
-                sum[i] += v['value'][i] / n
-
-        self.centroid = sum
-        return sum
-
-    def distance(self, point):
-        """-----------------------------------------------------------------------------------------
-        calculate distance of point from centroid
-
-        :param point: list  data vector for a data item
-        :return: list       distance to centroid, same dimension as point
-        -----------------------------------------------------------------------------------------"""
-        d = 0.0
-        c = self.centroid
-        for i in range(len(c)):
-            d += abs(c[i] - point['value'][i])
-
-        return d
-
-
 # --------------------------------------------------------------------------------------------------
 # main
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    k = 7
-    group = []
-    for g in range(k):
-        group.append(Group())
+    k = 6
+
+    # read labels
+    rownames = []
+    origlabel = {}
+    labelfile = 'data/labels.txt'
+    label = open(labelfile, 'r')
+    n = 0
+    for line in label:
+        i, id = line.rstrip().split(',')
+        cut = id.find('.')
+        linepos = id.find('_')
+        if linepos > -1 and linepos < cut:
+            cut = linepos
+        name = id[:cut]
+
+        if name not in origlabel:
+            origlabel[name] = n
+            n += 1
+        rownames.append(origlabel[name])
+
+    label2name = []
+    for gname in origlabel:
+        label2name.append(gname)
+
+    label.close()
 
     # read data
-    datafile = 'data/latent.txt'
+    datafile = 'data/latent2.txt'
     datain = open(datafile, 'r')
     data = []
     for line in datain:
@@ -77,6 +48,7 @@ if __name__ == '__main__':
         item = [float(v) for v in field[1:]]
         data.append(item)
 
+    datain.close()
     datamat = np.array(data)
     print(f'values read from {datafile}: {len(data)}')
 
@@ -86,5 +58,32 @@ if __name__ == '__main__':
     centers = kmeans.cluster_centers_
     print(f'{labels}\n{centers}')
     print(f'error: {kmeans.inertia_:.2f}')
+
+    print('labels')
+    for g in origlabel:
+        print(f'{origlabel[g]}\t{g}')
+
+    # Plot the data points and cluster centers
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    scatter = ax.scatter(datamat[:, 1], datamat[:, 2], datamat[:, 3], c=rownames, cmap='Dark2')
+    ax.scatter(centers[:, 1], centers[:, 2], centers[:, 3], marker='x', color='black', s=50)
+    plt.title('K-Means Clustering')
+    ax.set_xlabel('Feature 1')
+    ax.set_ylabel('Feature 2')
+    ax.set_zlabel('Feature 3')
+
+    # handles, labels = ax.get_legend_handles_labels()
+    s = scatter.legend_elements()
+    legend1 = ax.legend(*scatter.legend_elements(), loc="best", title="Groups")
+    ax.add_artist(legend1)
+    # ax.legend(labels=label2name)
+    tt = legend1.get_texts()
+    t = 0
+    for lab in label2name:
+        legend1.get_texts()[t].set_text(lab)
+        t += 1
+
+    plt.show(block=True)
 
     exit(0)
